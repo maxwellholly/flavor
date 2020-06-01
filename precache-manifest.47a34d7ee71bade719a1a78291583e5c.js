192720f2116cbc22125e1c0dfb630b139c2ffc28
Holly Maxwell <hollymaxwell@Hollys-MacBook-Pro.local>
2020-06-01 10:49:44 -0700

    Updates

---
+++ precache-manifest.47a34d7ee71bade719a1a78291583e5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e38c56430528ff4a6273d47f98a01dae",
+    "url": "/flavor/index.html"
+  },
+  {
+    "revision": "c7f0558de6638d10b52e",
+    "url": "/flavor/static/css/main.5f42f361.chunk.css"
+  },
+  {
+    "revision": "5dc77bb29f4255f65831",
+    "url": "/flavor/static/js/2.e1ea8343.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/flavor/static/js/2.e1ea8343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7f0558de6638d10b52e",
+    "url": "/flavor/static/js/main.1510d6f1.chunk.js"
+  },
+  {
+    "revision": "0809fe24c9b5a684165c",
+    "url": "/flavor/static/js/runtime-main.163c0dd6.js"
+  },
+  {
+    "revision": "5f680ee14f410f1dda7da09f2cdfaf3c",
+    "url": "/flavor/static/media/flavorgarlic.5f680ee1.png"
+  }
+]);
